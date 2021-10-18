a4b8cc994e5a34574f36b217b94d327652c09f6d
amadejsenk <amadej.senk@indigo.si>
2021-10-18 18:53:55 +0200

    Fix function parameters in controller for api/region/lastweek endpoint

--- sledilnikCovid/Controllers/regionController.cs
+++ sledilnikCovid/Controllers/regionController.cs
@@ -22,9 +22,9 @@ namespace sledilnikCovid.Api.Controllers
         /// <summary>
         /// Returns all cases with information about date and region based data
         /// </summary>
-        /// <param name="region"></param>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
+        /// <param name="region">Filter cases based on this region.</param>
+        /// <param name="from">Filter cases from this date.</param>
+        /// <param name="to">Filter cases to this date.</param>
         /// <returns>A list of cases</returns>
         [HttpGet]
         [Route("cases")]
@@ -36,15 +36,12 @@ namespace sledilnikCovid.Api.Controllers
         }
 
         /// <summary>
-        /// Returns a list of regions with their sum of active cases for the past 7 days
+        /// Returns a list of regions with their sum of active cases for the past 7 days.
         /// </summary>
-        /// <param name="region"></param>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
         /// <returns>A list of regions and appropriate sums</returns>
         [HttpGet]
         [Route("lastweek")]
-        public async Task<ActionResult<List<LastweekDto>>> GetLastweek(string? region, DateTime? from, DateTime? to)
+        public async Task<ActionResult<List<LastweekDto>>> GetLastweek()
         {
             var data = await _regionService.FetchDataLastWeek();
             return Ok(data);
